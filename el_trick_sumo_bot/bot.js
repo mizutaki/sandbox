9019498d9962407e9f60c6ff54c04b4839adfc01
mizutaki <mizutaki4@gmail.com>
2016-03-27 22:46:35 +0900

    tweet forty eight hand

--- el_trick_sumo_bot/bot.js
+++ el_trick_sumo_bot/bot.js
@@ -1,18 +1,31 @@
 var Apiconf = require('./apiconf');
 var twitter = require('twitter');
-  
-var conf = new Apiconf(); 
-
-var bot = new twitter({	
-	consumer_key        : conf.consumer_key,
-	consumer_secret     : conf.consumer_secret,
-	access_token_key    : conf.access_token_key,
-	access_token_secret : conf.access_token_secret
-});
+var fs = require('fs');
 
 
-bot.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response){
-  if(error) throw error;
-  console.log(tweet);  // Tweet body.
-  console.log(response);  // Raw response object.
-});
+
+
+
+var tweet = function() {
+  var message;
+  fs.readFile('./script/kimarite.data', 'utf8', function(err,text) {
+    var arr  = text.split("\n");
+    var message = arr[Math.floor(Math.random() * arr.length)];
+    
+    var conf = new Apiconf();
+    var bot = new twitter({
+      consumer_key        : conf.consumer_key,
+      consumer_secret     : conf.consumer_secret,
+      access_token_key    : conf.access_token_key,
+      access_token_secret : conf.access_token_secret
+    });
+
+    bot.post('statuses/update', {status : message },  function(error, tweet, response){
+      if(error) throw error;
+      console.log(tweet);  // Tweet body.
+      console.log(response);  // Raw response object.
+    });
+  });
+}
+
+tweet();
