bde41a906bc30f47ba2809dfc360b25b43caaba1
mizutaki <mizutaki4@gmail.com>
2016-04-09 22:26:00 +0900

    chat, chatchat

---
+++ node_chat/app.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var server = require('http').createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type":"text/html"});
+  var output = fs.readFileSync("./index.html", "utf-8");
+  res.end(output);
+}).listen(1234);
+var io = require('socket.io').listen(server);
+
+var userHash = {};
+
+
+io.sockets.on('connection', function(socket) {
+  //接続開始（他ユーザへ通知）
+  socket.on('connected', function(name) {
+    var msg = name + "が入室しました";
+    userHash[socket.id] = name;
+    io.sockets.emit('publish', {value:msg});
+  });
+  //メッセージ送信
+  socket.on('publish', function(data) {
+    io.sockets.emit('publish', {value:data.value});
+  });
+  //接続終了組み込みイベント
+  socket.on('disconnect', function() {
+    if(userHash[socket.id]) {
+      var msg = userHash[socket.id] + "が提出しました";
+      delete userHash[socket.id];
+      io.sockets.emit('publish',{value:msg});
+    }
+  });
+});
