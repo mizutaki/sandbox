df55f355fc859a58d5f567db004b85cececdf113
mizutaki <mizutaki4@gmail.com>
2016-07-16 15:57:32 +0900

    add chapter4 and 5

---
+++ rails/chapter5/asagao/app/controllers/members_controller.rb
@@ -0,0 +1,29 @@
+class MembersController < ApplicationController
+  def index
+    @members = Member.order("number")
+  end
+  #会員情報の詳細
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def search
+    @members = Member.search(params[:q])
+    render "index"
+  end
+end
