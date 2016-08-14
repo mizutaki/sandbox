6b40cea6a9ef7ff13ca6c09fddc1f0c8c79a42d3
mizutaki <mizutaki4@gmail.com>
2016-08-14 21:35:33 +0900

    Added the edit action and view of Entry.

--- rails_entryβ/entry_b/app/controllers/entries_controller.rb
+++ rails_entryβ/entry_b/app/controllers/entries_controller.rb
@@ -22,6 +22,20 @@ class EntriesController < ApplicationController
     @entry = Entry.find(params[:id])
   end
 
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    @entry.assign_attributes(entry_params)
+    if @entry.save
+      redirect_to @entry, notice: "更新しました。"
+    else
+      render "edit"
+    end
+  end
+
   private
   def entry_params
     params.require(:entry).permit(:title, :body, :posted_at)
