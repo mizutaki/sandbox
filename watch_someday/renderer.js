e1b8c62aae64b35189afa2cc544875b5832769ac
mizutaki <mizutaki4@gmail.com>
2016-10-09 17:57:59 +0900

    add insert sql.

--- watch_someday/renderer.js
+++ watch_someday/renderer.js
@@ -1,15 +1,19 @@
 (function() {
-  //DB処理
-  var clickEventHandler = function() {
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
+  var mysql = require('mysql');
+  var connection = function() {
+    var conn = mysql.createConnection({
       host:'localhost',
       user:'root',
       password:'root',
       database:'test'
     });
-    connection.connect();
-    connection.query('SELECT * FROM testTable;', function(err, rows, fileds) {
+    return conn;
+  };
+
+  //DB処理
+  var showEventHandler = function() {
+    connection().connect();
+    connection().query('SELECT * FROM testTable;', function(err, rows, fileds) {
       if (err) throw err;
       if (rows.lengh > 0) {
         console.log(lows[0].message);
@@ -17,6 +21,20 @@
         console.log('なにもない');
       }
     });
+    connection().end();
   };
-  document.querySelector('#showButton').addEventListener('click', clickEventHandler);
+  
+  var addEventHandler = function() {
+    var text = document.querySelector('#text').value;
+    var sql = 'INSERT INTO test.testTable VALUES(?)';
+    connection().connect();
+    connection().query(sql, [text], function(err, result) {
+      if (err) throw err;
+      if (result.affectedRows > 0) console.log('success insert!');
+    });
+    connection().end();
+  };
+
+  document.querySelector('#showButton').addEventListener('click', showEventHandler);
+  document.querySelector('#addButton').addEventListener('click', addEventHandler);
 }());
