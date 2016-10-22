db8fc583af3ed4e432170a666f6dbdebc1b1028d
mizutaki <mizutaki4@gmail.com>
2016-10-22 11:16:16 +0900

    ver3

--- playground/Practical Object-Oriented Design in Ruby/3/Gear.rb
+++ playground/Practical Object-Oriented Design in Ruby/3/Gear.rb
@@ -1,16 +1,21 @@
 class Gear
-  attr_reader :chainring, :cog, :wheel
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog, wheel)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @wheel = wheel
+    @rim = rim
+    @tire = tire
   end
 
   def gear_inches
     ratio * wheel.diameter
   end
 
+  def wheel
+    @wheel ||= Wheel.new(rim, tire)
+  end 
+  
   def ratio
     chainring / cog.to_f
   end
