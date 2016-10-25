56aeeea40d3dedc0b883cb64a6a40ec9a9d91d42
mizutaki <mizutaki4@gmail.com>
2016-10-25 22:53:16 +0900

    6 ver4

--- playground/Practical Object-Oriented Design in Ruby/6/Bicycle.rb
+++ playground/Practical Object-Oriented Design in Ruby/6/Bicycle.rb
@@ -1,13 +1,17 @@
 class Bicycle
-  attr_reader :style, :size, :tap_color,
-              :front_shock, :rear_shock
+  attr_reader :size
+
+  def initialize(args={})
+    @size = args[:size]
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :tap_color
 
   def initialize(args)
-    @style = args[:style]
-    @size = args[:size]
     @tap_color = args[:tap_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
+    super(args)
   end
 
   def spares
@@ -21,14 +25,31 @@ class Bicycle
         rear_shock: rear_shock}
     end
   end
-
 end
 
-bike = Bicycle.new(
-        style:       :mountain,
-        size:        'M',
-        front_shock: 'Monitou',
-        rear_shock:  'Fox')
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
 
-p bike.size
-p bike.spares
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+road_bike = RoadBike.new(
+        size:        'M',
+        type_color: 'red')
+
+p road_bike.size
+
+mountain_bike = MountainBike.new(
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox')
+
+p mountain_bike.size
