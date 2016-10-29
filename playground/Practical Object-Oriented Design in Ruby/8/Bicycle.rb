05cf177d7bb62aa01684aaa9958c957c1e9c6d33
mizutaki <mizutaki4@gmail.com>
2016-10-30 06:54:20 +0900

    8 ver2

--- playground/Practical Object-Oriented Design in Ruby/8/Bicycle.rb
+++ playground/Practical Object-Oriented Design in Ruby/8/Bicycle.rb
@@ -12,82 +12,46 @@ class Bicycle
 end
 
 class Parts
+  attr_reader :parts
   attr_reader :chain, :tire_size
 
-  def initialize(args={})
-    @chain = args[:chain] || default_chain
-    @tire_size = args[:tire_size] || default_tire_size
-    post_initialize(args)
+  def initialize(parts)
+    @parts = parts
   end
 
   def spares
-    {tire_size: tire_size,
-      chain: chain}.merge(local_spares)
-  end
-  
-  def default_tire_size
-    raise NotImplementedError, "This #{self.class} cannot respond to:"
-  end
-
-  def post_initialize(args)
-    nil
-  end
-
-  def local_spares
-    {}
-  end
-
-  def default_chain
-    '10-speed'
-  end
-
-  
-end
-
-class RoadBikeParts < Parts
-  attr_reader :tape_color
-
-  def post_initialize(args)
-    @tape_color = args[:tape_color]
-  end
-
-  def local_spares
-    { tape_color: tape_color }
-  end
-
-  def default_tire_size
-    '23'
+    parts.select { |part| part.needs_spare }
   end
 end
 
-class MountainBikeParts < Parts
-  attr_reader :front_shock, :rear_shock
+class Part
+  attr_reader :name, :description, :needs_spare
 
-  def post_initialize(args)
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
-  end
-
-  def local_spares
-    {rear_shock: rear_shock}
-  end
-
-  def default_tire_size
-    '2.1'
+  def initialize(args)
+    @name = args[:name]
+    @description = args[:description]
+    @needs_spare = args.fetch(:needs_spare, true)
   end
 end
+
+chain = Part.new(name: 'chain', description: '10-speed')
+road_tire = Part.new(name: 'tire_size', description: '23')
+tape = Part.new(name: 'tape_color', description: 'red')
+mountain_tire = Part.new(name: 'tire_size', description: '2.1')
+rear_shock = Part.new(name: 'read_shock', description: 'FOX')
+front_shock = Part.new(
+  name: 'front_shock',
+  description: 'Manitou',
+  needs_spare: false)
 
 road_bike = Bicycle.new(
-  size: 'L',
-  parts: RoadBikeParts.new(tape_color: 'red'))
-
+  size:'L',
+  parts: Parts.new([chain, road_tire, tape]))
 p road_bike.size
-
 p road_bike.spares
 
 mountain_bike = Bicycle.new(
   size: 'L',
-  parts: MountainBikeParts.new(rear_shock: 'FOX'))
-
+  parts: Parts.new([chain, mountain_tire, front_shock, rear_shock]))
 p mountain_bike.size
 p mountain_bike.spares
