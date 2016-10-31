1943a01624b0c48bd6c485276c68ea797d2b7772
mizutaki <mizutaki4@gmail.com>
2016-10-31 21:52:51 +0900

    8 ver4

--- playground/Practical Object-Oriented Design in Ruby/8/Bicycle.rb
+++ playground/Practical Object-Oriented Design in Ruby/8/Bicycle.rb
@@ -11,55 +11,24 @@ class Bicycle
   end
 end
 
+require 'forwardable'
 class Parts
-  attr_reader :parts
-  attr_reader :chain, :tire_size
+  extend Forwardable
+  def_delegators :@parts, :size, :each
+  include Enumerable
 
   def initialize(parts)
     @parts = parts
   end
 
   def spares
-    parts.select { |part| part.needs_spare }
+    select { |part| part.needs_spare}
   end
 end
-
-class Part
-  attr_reader :name, :description, :needs_spare
-
-  def initialize(args)
-    @name = args[:name]
-    @description = args[:description]
-    @needs_spare = args.fetch(:needs_spare, true)
-  end
-end
-
-chain = Part.new(name: 'chain', description: '10-speed')
-road_tire = Part.new(name: 'tire_size', description: '23')
-tape = Part.new(name: 'tape_color', description: 'red')
-mountain_tire = Part.new(name: 'tire_size', description: '2.1')
-rear_shock = Part.new(name: 'read_shock', description: 'FOX')
-front_shock = Part.new(
-  name: 'front_shock',
-  description: 'Manitou',
-  needs_spare: false)
-
-road_bike = Bicycle.new(
-  size:'L',
-  parts: Parts.new([chain, road_tire, tape]))
-p road_bike.size
-p road_bike.spares
-
-mountain_bike = Bicycle.new(
-  size: 'L',
-  parts: Parts.new([chain, mountain_tire, front_shock, rear_shock]))
-p mountain_bike.size
-p mountain_bike.spares
 
 require 'ostruct'
 module PartsFactory
-  def self.build(config,
-                 parts_class = Parts)
+  def self.build(config, parts_class = Parts)
     parts_class.new(
       config.collect {|part_config|
         create_part(part_config)})
@@ -84,5 +53,12 @@ mountain_config =
  ['front_shock', 'Manitou', false],
  ['rear_shock', 'FOX']]
 
-p road_parts = PartsFactory.build(road_config)
-p mountain_parts = PartsFactory.build(mountain_config)
+p road_bike = Bicycle.new(
+  size: 'L', 
+  parts: PartsFactory.build(road_config))
+p road_bike.spares
+
+p mountain_bike = Bicycle.new(
+  size: 'L', 
+  parts: PartsFactory.build(mountain_config))
+p mountain_bike.spares
