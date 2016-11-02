861f58e91bbf33d3da1f51e54c4e3f07a5c2d501
mizutaki <mizutaki4@gmail.com>
2016-11-03 06:58:24 +0900

    9 ver4

--- playground/Practical Object-Oriented Design in Ruby/9/gear.rb
+++ playground/Practical Object-Oriented Design in Ruby/9/gear.rb
@@ -1,10 +1,11 @@
 class Gear
-  attr_reader :chainring, :cog, :wheel
+  attr_reader :chainring, :cog, :wheel, :observer
 
   def initialize(arg)
     @chainring = arg[:chainring]
     @cog = arg[:cog]
     @wheel = args[:wheel]
+    @observer = args[:observer]
   end
 
   def gear_inches
@@ -13,6 +14,20 @@ class Gear
   
   def ratio
     chainring / cog.to_f
+  end
+
+  def set_cog(new_cog)
+    @cog = new_cog
+    changed
+  end
+
+  def set_chainring(new_chainring)
+    @chainring = new_chainring
+    changed
+  end
+
+  def changed
+    observer.changed(chainring, cog)
   end
 end
 
--- playground/Practical Object-Oriented Design in Ruby/9/test_gear.rb
+++ playground/Practical Object-Oriented Design in Ruby/9/test_gear.rb
@@ -15,14 +15,32 @@ class DiameterDouble
 end
 
 class GearTest < MiniTest::Test
+  def setup
+    @observer = MiniTest::Mock.new
+    @gear = Gear.new(
+              chainring: 52,
+              cog: 11,
+              observer: @observer)
+  end
+
+  def test_notifies_observers_when_cog_change
+    @observer.expect(:changed, true, [52, 27])
+    @gear.set_cog(27)
+    @observer.verify
+  end
+
+  def test_notifies_observers_when_chainring_change
+    @observer.expect(:changed, true, [42, 11])
+    @gear.set_cog(42)
+    @observer.verify
+  end 
+
   def test_calculates_gear_inches
     gear = Gear.new(
       chainring: 52,
       cog: 11,
-      wheel: DiameterDouble.new
+      wheel: DiameterDouble.new)
 
-    assert_in_delta(137.1,
-                    gear.gear_inches,
-                    0.01)
+    assert_in_delta(137.1, gear.gear_inches, 0.01)
   end
 end
