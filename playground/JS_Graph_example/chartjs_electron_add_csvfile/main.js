751c5dd034bfcb88c3c6b8aad118f8a227b1ccc2
mizutaki <mizutaki4@gmail.com>
2016-11-26 20:56:21 +0900

    Write log at startup.

--- playground/JS_Graph_example/chartjs_electron_add_csvfile/main.js
+++ playground/JS_Graph_example/chartjs_electron_add_csvfile/main.js
@@ -9,14 +9,38 @@ mb.on('ready', function ready () {
 })
 
 let exec = require('child_process').exec;
+let fs = require('fs');
 let command = `wget -O nul -a wget.log -t 3 -T 20 http://www.asahi-net.or.jp/~yh8n-wke/image/speed_imagec1.jpg
 wget -O nul -a wget.log -t 3 -T 20 http://bnr.on.arena.ne.jp/image/speed_imagec1.jpg
 grep saved wget.log >> result.log
 rm wget.log
 rm nul`;
 exec(command, (err, stdout, stderr) => {
-  if (err) { console.log(err); }
-  console.log(stdout);
+  if (err) { console.err(err); }
+  fs.readFile('result.log', (err, data) => {
+    if (err) { console.err(err); }
+    var str = data.toString();
+    var tmp = str.split('\n')
+    for (var t of tmp) {
+      if (t === '') continue;
+      var tmpDate = t.match(/[\d]{4}-[\d]{2}-[\d]{2} [\d]{2}:[\d]{2}:[\d]{2}/);
+      var tmpSize = t.match(/\d*\.*\d* (KB|MB)/);
+      var date = tmpDate[0];
+      var size = tmpSize[0];
+      if (size.includes('KB')) {
+        size = Number.parseInt(size) / 1024;
+      } else {
+        size = Number.parseFloat(size);
+      }
+      var writeData = `${date},${size}\n`;
+      fs.appendFile('data.csv', writeData, (err) => {
+        if (err) { console.err(err); }
+      });
+    }
+    fs.unlink('result.log', (err) => {
+      if (err) { console.err(err); }
+    });
+  });
 });
 
 /*
